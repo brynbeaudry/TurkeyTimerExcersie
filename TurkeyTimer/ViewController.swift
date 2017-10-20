e01c8de3940954707391c0d546dc2b2612434b78
Bryn Beaudry <babeaudry@gmail.com>
2017-10-20 14:59:15 -0700

    Initial Commit

---
+++ TurkeyTimer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TurkeyTimer
+//
+//  Created by Bryn Beaudry on 2017-10-20.
+//  Copyright © 2017 Bryn Beaudry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
